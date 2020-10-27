ff5a1a1f8032e61e9ae304240d6506b1d5ca029c
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2020-10-27 20:42:39 +0100

    Reload button

--- simpson.js
+++ simpson.js
@@ -1,23 +1,31 @@
 // This function loads pokemon data from the Pokemon API
 function fecthSimpsonJson() {
-    // Feel free to download this HTML and edit it, to use another Pokemon ID
-    const url = `https://simpsons-quotes-api.herokuapp.com/quotes`;
-    axios.get(url)
-      .then(function(response) {
-        return response.data; // response.data instead of response.json() with fetch
-      })
-      .then(function(quote) {
-        console.log('data decoded from JSON:', quote);
-        
+  // Feel free to download this HTML and edit it, to use another Pokemon ID
+  const url = `https://simpsons-quotes-api.herokuapp.com/quotes`;
+  axios
+    .get(url)
+    .then(function (response) {
+      return response.data; // response.data instead of response.json() with fetch
+    })
+    .then(function (quote) {
+      console.log("data decoded from JSON:", quote);
 
-        // Build a block of HTML
-        const quoteHtml = `
+      const button = document.querySelector("button");
+
+      button.addEventListener("click", (event) => {
+        {
+          location.reload();
+        }
+      });
+
+      // Build a block of HTML
+      const quoteHtml = `
           <p><strong>${quote[0].quote}</strong></p>
           <p>${quote[0].character}<p>
           <img src="${quote[0].image}"/>
         `;
-        document.querySelector('#quote').innerHTML = quoteHtml;
-      });
-  }
-  
-  fecthSimpsonJson();
+      document.querySelector("#quote").innerHTML = quoteHtml;
+    });
+}
+
+fecthSimpsonJson();
